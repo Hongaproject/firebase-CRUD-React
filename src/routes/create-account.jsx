6b10e393e09d79f2fb632ab62c7768bf20747092
Hongaproject <tkwlscjq99@gmail.com>
2024-05-10 14:35:39 +0900

    회원가입 부분 설계 onChange, onSubmit 추가

--- src/routes/create-account.jsx
+++ src/routes/create-account.jsx
@@ -1,9 +1,52 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    
+`;
+
+const Form = styled.form`
+    
+`;
+
+const Input = styled.input`
+    
+`;
+
 function CreateAccount() {
+    
+    const [isLoading, setLoading] = useState(false);
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onChange = (e) => { // 이렇게 하면 input이 변경 되었을 때 어떤 input이 변경되었는지 찾을 수 있다.
+        const {target: {name, value}} = e;
+        if(name === "name"){ // 뭔지를 아니까 state의 값을 set에 넣어줄수있다.
+            setName(value)
+        }else if(name === "email") {
+            setEmail(value)
+        }else if(name === "password") {
+            setPassword(value)
+        }
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log(name, email, password);
+    }
+
     return(
-        <div>
-            생성화면 입니다.
-        </div>
+        <Wrapper>
+            <Form onSubmit={onSubmit}>
+                <Input name="name" value={name} onChange={onChange} placeholder="Name" type="text" required />
+                <Input name="email" value={email} onChange={onChange} placeholder="Email" type="email" required />
+                <Input name="password" value={password} onChange={onChange} placeholder="Password" type="password" required />
+                <Input type="submit" value="Create Account" />
+            </Form>
+        </Wrapper>
     );
 }
 
 export default CreateAccount;
+
