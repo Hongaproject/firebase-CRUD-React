2f5f110e1313100e1119775821d10b5b9729abc6
Hongaproject <tkwlscjq99@gmail.com>
2024-05-10 15:57:16 +0900

    로그아웃 기능 추가

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,7 +1,15 @@
+import { auth } from "../firebase";
+
 function Home() {
+
+    const logOut = () => {
+        auth.signOut();
+    }
+
     return(
         <div>
             홈 입니다.
+            <button onClick={logOut}>로그아웃</button>
         </div>
     );
 }
